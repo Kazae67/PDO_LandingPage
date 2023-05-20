05e9c62ad9427ddc2a4b52b731a9a6e06e785439
Kazae67 <Kazae.zaeka@gmail.com>
2023-05-20 22:23:36 +0200

    ajout du DNS

--- db-functions.php
+++ db-functions.php
@@ -1,3 +1,14 @@
 <?php
 
-?>
+$host = 'localhost';
+$dbname = 'pricing';
+$username = 'root';
+$password = '';
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $err) {
+    echo "Erreur de connexion à la base de données : " . $err->getMessage();
+    exit;
+}
