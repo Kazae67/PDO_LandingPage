a1bd8113e87f145f501015a701642657dd5a5f10
Kazae67 <Kazae.zaeka@gmail.com>
2023-05-20 22:37:30 +0200

    Ajout des commentaires DN

--- db-functions.php
+++ db-functions.php
@@ -1,10 +1,21 @@
 <?php
 
+/**
+ *@DNSServer
+ *Nom du domaine
+ *Nom d'utilisateur
+ *Password
+ */
 $host = 'localhost';
 $dbname = 'pricing';
 $username = 'root';
 $password = '';
 
+
+/**
+ *@errorCode Affiche une erreur si la connexion ne s'établie pas
+ *On aurait pu mettre dans ça dans une Class pour l'utiliser comme fonction
+ */
 try {
     $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
