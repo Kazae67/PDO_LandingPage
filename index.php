e28d11e21af1dad7920462dc5db93f6f630b993f
Kazae67 <Kazae.zaeka@gmail.com>
2023-05-21 05:01:44 +0200

    Ajout de la condition si ça excède 1000mb

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $result = $db->query($query);
 if ($result->rowCount() > 0) {
     // Parcourir les résultats
     while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-        $formule = $row['formule'];
+        $formule = $row['formule']; 
         $prix = $row['prix'];
         $reduction = $row['reduction'];
         $afficherReduction = $row['afficher_reduction'];
@@ -19,16 +19,25 @@ if ($result->rowCount() > 0) {
         $domain = $row['domain'];
         $hidden_fees = $row['hidden_fees'];
 
+
+        // Conversion de la valeur de onlinespace en GB si elle dépasse 1000
+        if ($onlinespace > 999) {
+            $onlinespace = round($onlinespace / 1000, 1) . 'GB';
+        } else {
+            $onlinespace = $onlinespace . 'MB';
+        }
+
         // Afficher les formules de pricing dans le format souhaité
         echo "<div class='pricing-box'>";
-        echo "<h3>$formule</h3>";
+        echo "<h2>$formule</h2>";
         echo "<p>Prix : $prix €</p>";
 
+        // Condition pour afficher la réduction [0 :? 1]
         if ($afficherReduction && $reduction > 0) {
             echo "<p>Réduction : -$reduction €</p>";
         }
 
-        // [TÂCHE IMPORTANTE ne pas oublier] Trouvez une condition pour afficher soit GB soit MB à la fin
+        // Typage catégories des formules de pricing 
         echo "<p>Bandwidth : $bandwidth</p>";
         echo "<p>Onlinespace : $onlinespace</p>";
         echo "<p>Support : " . ($support ? "Yes" : "No") . "</p>";
@@ -40,5 +49,6 @@ if ($result->rowCount() > 0) {
 } else {
     echo "Aucune formule de la base de donnée pricing trouvée.";
 }
+
 
 ?>
