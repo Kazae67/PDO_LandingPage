eefae5eda7eb59a9b5f04e33f735bfeb083f8aa4
Kazae67 <Kazae.zaeka@gmail.com>
2023-05-27 00:30:37 +0200

    form-style.css entièrement revisité

--- admin.php
+++ admin.php
@@ -24,67 +24,73 @@ $formules = array(
  * (4) Les balises <select> pour afficher des options
  * 
  */
-foreach ($formules as $formule) { // (1)
-    ?>
-    <!-- Début Formulaire -->
-    <form action="admin.php" method="POST"> <!--(2)-->
-        <h2><?php echo $formule; ?></h2>
-        <input type="hidden" name="formule" value="<?php echo $formule; ?>">  <!--(3)-->
-
-         <!-- Champ "Name" -->
-        <label for="name">Name:</label>
-        <input type="text" name="name" required>
-        <br>
-
-        <!-- Champ "Price" -->
-        <label for="price">Price:</label>
-        <input type="text" name="price" required>
-        <br>
-
-        <!-- Champ "Sale" -->
-        <label for="sale">Sale:</label>
-        <input type="text" name="sale" required>
-        <br>
-
-        <!-- Champ "Bandwidth" -->
-        <label for="bandwidth">Bandwidth:</label>
-        <input type="text" name="bandwidth" required>
-        <br>
-
-        <!-- Champ "Onlinespace" -->
-        <label for="onlinespace">Onlinespace:</label>
-        <input type="text" name="onlinespace" required>
-        <br>
-
-        <!-- Champ "Support" -->
-        <label for="support">Support:</label>
-        <select name="support" required> <!--(4)-->
-            <option value="Yes">Yes</option>
-            <option value="No">No</option>
-        </select>
-        <br>
-
-        <!-- Champ "Domain" -->
-        <label for="domain">Domain:</label>
-        <input type="text" name="domain" required>
-        <br>
-
-        <!-- Champ "Hidden fees" -->
-        <label for="hidden_fees">Hidden fees:</label>
-        <select name="hidden_fees" required>
-            <option value="Yes">Yes</option>
-            <option value="No">No</option>
-        </select>
-        <br>
-
-        <!-- Champ "Commande" -->
-        <label for="commande">Commande:</label>
-        <input type="text" name="commande" required>
-        <br>
-
-        <!-- Bouton de soumission -->
-        <input type="submit" name="update" value="Update">
-    </form>
-    <?php
-}
 ?>
+<div class="admin-form-container">
+    <?php
+    foreach ($formules as $formule) { // (1)
+        ?>
+        <!-- Début Formulaire -->
+        <div class="admin-form-box">
+            <form action="admin.php" method="POST"> <!--(2)-->
+                <h2><?php echo $formule; ?></h2>
+                <input type="hidden" name="formule" value="<?php echo $formule; ?>">  <!--(3)-->
+
+                <!-- Champ "Name" -->
+                <label for="name">Name:</label>
+                <input type="text" name="name" required>
+                <br>
+
+                <!-- Champ "Price" -->
+                <label for="price">Price:</label>
+                <input type="text" name="price" required>
+                <br>
+
+                <!-- Champ "Sale" -->
+                <label for="sale">Sale:</label>
+                <input type="text" name="sale" required>
+                <br>
+
+                <!-- Champ "Bandwidth" -->
+                <label for="bandwidth">Bandwidth:</label>
+                <input type="text" name="bandwidth" required>
+                <br>
+
+                <!-- Champ "Onlinespace" -->
+                <label for="onlinespace">Onlinespace:</label>
+                <input type="text" name="onlinespace" required>
+                <br>
+
+                <!-- Champ "Support" -->
+                <label for="support">Support:</label>
+                <select name="support" required> <!--(4)-->
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option>
+                </select>
+                <br>
+
+                <!-- Champ "Domain" -->
+                <label for="domain">Domain:</label>
+                <input type="text" name="domain" required>
+                <br>
+
+                <!-- Champ "Hidden fees" -->
+                <label for="hidden_fees">Hidden fees:</label>
+                <select name="hidden_fees" required>
+                    <option value="Yes">Yes</option>
+                    <option value="No">No</option>
+                </select>
+                <br>
+
+                <!-- Champ "Commande" -->
+                <label for="commande">Commande:</label>
+                <input type="text" name="commande" required>
+                <br>
+
+                <!-- Bouton de soumission -->
+                <input type="submit" name="update" value="Update">
+            </form>
+        </div>
+        <?php
+    }
+    ?>
+</div>
