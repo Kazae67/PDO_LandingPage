2671094a5a10a5bfad0d429106ec2303dac2f968
Kazae67 <Kazae.zaeka@gmail.com>
2023-05-27 15:38:57 +0200

    Grande mise à jour 'Remplissage automatique sur le formulaire admin.php'

--- admin.php
+++ admin.php
@@ -9,6 +9,10 @@ require_once 'DB/db-functions.php';
 
 // Function(s)
 updateForm(connection());
+// Récupération des valeurs à partir de la base de données
+$db = connection();
+$statement = $db->query("SELECT * FROM pricing_db");
+$data = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 // Formules
 $formules = array(
@@ -27,63 +31,93 @@ $formules = array(
 ?>
 <div class="admin-form-container">
     <?php
-    foreach ($formules as $formule) { // (1)
+foreach ($formules as $formule) {
+    // Recherche de la correspondance dans les données de la base de données
+    $matchingData = array_filter($data, function ($row) use ($formule) {
+        return $row['formule'] === $formule;
+    });
+
+    // Vérification si une correspondance a été trouvée
+    if (!empty($matchingData)) {
+        $row = reset($matchingData); 
+
+        $name = $row['formule'];
+        $price = $row['prix'];
+        $sale = $row['reduction'];
+        $bandwidth = $row['bandwidth'];
+        $onlinespace = $row['onlinespace'];
+        $support = $row['support'] ? 'Yes' : 'No';
+        $domain = $row['domain'];
+        $hidden_fees = $row['hidden_fees'] ? 'Yes' : 'No';
+        $commande = $row['commande'];
+    } else {
+        // Utilise des valeurs par défaut si aucune correspondance n'a été trouvée dans la base de données
+        $name = $formule;
+        $price = '';
+        $sale = '';
+        $bandwidth = '';
+        $onlinespace = '';
+        $support = 'Yes';
+        $domain = '';
+        $hidden_fees = 'Yes';
+        $commande = '';
+    }
         ?>
         <!-- Début Formulaire -->
         <div class="admin-form-box">
             <form action="admin.php" method="POST"> <!--(2)-->
-                <h2><?php echo $formule; ?></h2>
-                <input type="hidden" name="formule" value="<?php echo $formule; ?>">  <!--(3)-->
+                <h2><?php echo $name; ?></h2>
+                <input type="hidden" name="formule" value="<?php echo $name; ?>">  <!--(3)-->
 
                 <!-- Champ "Name" -->
                 <label for="name">Name:</label>
-                <input type="text" name="name" required>
+                <input type="text" name="name" value="<?php echo $name; ?>" required>
                 <br>
 
                 <!-- Champ "Price" -->
                 <label for="price">Price:</label>
-                <input type="text" name="price" required>
+                <input type="text" name="price" value="<?php echo $price; ?>" required>
                 <br>
 
                 <!-- Champ "Sale" -->
                 <label for="sale">Sale:</label>
-                <input type="text" name="sale" required>
+                <input type="text" name="sale" value="<?php echo $sale; ?>" required>
                 <br>
 
                 <!-- Champ "Bandwidth" -->
                 <label for="bandwidth">Bandwidth:</label>
-                <input type="text" name="bandwidth" required>
+                <input type="text" name="bandwidth" value="<?php echo $bandwidth; ?>" required>
                 <br>
 
                 <!-- Champ "Onlinespace" -->
                 <label for="onlinespace">Onlinespace:</label>
-                <input type="text" name="onlinespace" required>
+                <input type="text" name="onlinespace" value="<?php echo $onlinespace; ?>" required>
                 <br>
 
                 <!-- Champ "Support" -->
                 <label for="support">Support:</label>
                 <select name="support" required> <!--(4)-->
-                    <option value="Yes">Yes</option>
-                    <option value="No">No</option>
+                <option value="Yes" <?php if ($support === 'Yes') echo 'selected'; ?>>Yes</option>
+                <option value="No" <?php if ($support === 'No') echo 'selected'; ?>>No</option>
                 </select>
                 <br>
 
                 <!-- Champ "Domain" -->
                 <label for="domain">Domain:</label>
-                <input type="text" name="domain" required>
+                <input type="text" name="domain" value="<?php echo $domain; ?>" required>
                 <br>
 
                 <!-- Champ "Hidden fees" -->
                 <label for="hidden_fees">Hidden fees:</label>
                 <select name="hidden_fees" required>
-                    <option value="Yes">Yes</option>
-                    <option value="No">No</option>
+                <option value="Yes" <?php if ($hidden_fees === 'Yes') echo 'selected'; ?>>Yes</option>
+                <option value="No" <?php if ($hidden_fees === 'No') echo 'selected'; ?>>No</option>
                 </select>
                 <br>
 
                 <!-- Champ "Commande" -->
                 <label for="commande">Commande:</label>
-                <input type="text" name="commande" required>
+                <input type="text" name="commande" value="<?php echo $commande; ?>" required>
                 <br>
 
                 <!-- Bouton de soumission -->
